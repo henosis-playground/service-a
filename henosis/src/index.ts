adc91847fe91a47c329a866c0604fe4e2fec4421
Henosis Agent <henosis-agent@users.noreply.github.com>
2026-07-08 07:42:57 +0300

    Rename service-a api output

--- henosis/src/index.ts
+++ henosis/src/index.ts
@@ -2,9 +2,9 @@ import { defineComponent, envName, h } from "@henosis/platform-mock";
 
 export default defineComponent({
   outputs: h.object({
-    api: h.url(),
+    api_url: h.url(),
   }),
   build: (_ctx, env) => ({
-    api: `https://service-a-${envName(env)}.henosis.example`,
+    api_url: `https://service-a-${envName(env)}.henosis.example`,
   }),
 });
