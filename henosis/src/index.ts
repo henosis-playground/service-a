e7df79618097e6757a6642342d3260aeb0a93d43
Mike <42689366+MikeIvanichev@users.noreply.github.com>
2026-07-08 14:21:23 +0300

    Remove 'api' output from componentlol
    Remove the 'api' output from the component definition.

--- henosis/src/index.ts
+++ henosis/src/index.ts
@@ -2,7 +2,6 @@ import { defineComponent, envName, h } from "@henosis/platform-mock";
 
 export default defineComponent({
   outputs: h.object({
-    api: h.url(),
   }),
   build: (_ctx, env) => ({
     api: `https://service-a-${envName(env)}.henosis.example/api/v2/healthz`,
